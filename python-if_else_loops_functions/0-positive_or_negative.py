39086842c734869a6c8b27178be7392e42556e96
waseprisca0 <u.prisca@alustudent.com>
2023-02-08 08:42:00 -0800

    0-positive_or_negative.py

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    digit = number % 10
+else:
+    digit = ((number * -1) % 10) * -1
+print("Last digit of {} is {}".format(number, digit), end=" ")
+if digit > 5:
+       print("and is greater than 5")
+elif number == 0:
+       print("and is 0")
+else:
+       print("and is less than 6 and not 0")
+       
