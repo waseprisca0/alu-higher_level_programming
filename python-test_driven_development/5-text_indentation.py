13b4f4c32589af370d0a491feda998ee6105f00b
waseprisca0 <u.prisca@alustudent.com>
2023-04-12 05:57:11 -0700

    done

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-"""A function to sperate words in python"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    '''Fucntion to bring the texts'''
-    if type(text) != str:
+    """A function that prints two new lines after the char ...
+        :param text:
+        :type text:; string
+        :raise TypeError: if text is not a string
+        """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delim in ".:?":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)]
-        )
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    print("{}".format(text), end="")
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
