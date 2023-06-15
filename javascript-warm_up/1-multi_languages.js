71d67c2f52f9c6da45963de79c17f3fbeaf7f337
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2023-06-15 22:12:14 +0200

    Update 1-multi_languages.js

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const strg1 = 'C is fun';
-const strg2 = 'Python is cool';
-const strg3 = 'JavaScript is amazing';
-console.log(strg1);
-console.log(strg2);
-console.log(strg3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
