62542ec2e853b39ab6b4787aab6c0fccd73d42a2
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2023-06-15 22:17:38 +0200

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,17 @@
 #!/usr/bin/node
+const argsCount = process.argv.length;
+const numbers = [];
+switch (argsCount) {
+  case 2:
+  case 3:
+    console.log(0);
+    break;
 
-const argu = process.argv.slice(2);
-if (argu.length <= 1) {
-  console.log(0);
-} else {
-  console.log(argu.sort((a, b) => b - a)[1]);
+  default:
+    for (let i = 2; i < argsCount; i++) {
+      numbers.push(process.argv[i]);
+    }
+    numbers.sort((a, b) => b - a);
+    console.log(numbers[1]);
+    break;
 }
